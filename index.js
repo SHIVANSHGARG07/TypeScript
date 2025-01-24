0657065d9c4ec11da67b7511eb64dfdcb35568da
Shivansh Garg <shivanshgarg587@gmail.com>
2025-01-24 22:11:47 +0530

    hello

---
+++ index.js
@@ -0,0 +1,5 @@
+console.log("hello");
+var numbers = [1, 2, 3, 4];
+// number to string
+var stringNumbers = numbers.map(function (currEle) { return currEle.toString(); });
+console.log(stringNumbers);
