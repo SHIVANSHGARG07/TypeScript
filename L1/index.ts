89e887ab4c5966a24378dab0f28f38ed01d6d783
Shivansh Garg <shivansh@huzl.in>
2025-02-15 15:39:00 +0530

    second

---
+++ L1/index.ts
@@ -0,0 +1,7 @@
+console.log("hello");
+
+const numberss:number[] = [1,2,3,4];
+
+// number to string
+const stringNumberss:string[] = numberss.map((currEle:number)=>currEle.toString());
+console.log(stringNumbers);
--- index.ts
+++
@@ -1,7 +0,0 @@
-console.log("hello");
-
-const numbers:number[] = [1,2,3,4];
-
-// number to string
-const stringNumbers:string[] = numbers.map((currEle:number)=>currEle.toString());
-console.log(stringNumbers);
